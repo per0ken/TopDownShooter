f0c50f294ef978e3071e69023451c7b1b609b345
per0ken <peter.lukacs16@gmail.com>
2021-11-11 14:14:10 +0100

    design és a movement megcsinálva

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -16,20 +16,38 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.W))
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(-horizontalInput, 0, -verticalInput);
+        movementDirection.Normalize();
+
+        Vector3 movement = new Vector3(0.0f, verticalInput, horizontalInput);
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.S))
         {
             if (animator != null)
             {
                 animator.SetBool("IsRunning", false);
             }
         }
-        if (Input.GetKey(KeyCode.W))
+
+        //Rigidbody rb = GetComponent<Rigidbody>();
+        //GetComponent<Rigidbody>().velocity = movement * movementSpeed;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S))
         {
                     if(animator!=null)
             {
                 animator.SetBool("IsRunning", true);
             }
             transform.Translate(Vector3.forward * movementSpeed);
+        }
+
+
+        if (movementDirection != Vector3.zero)
+        {
+            transform.forward = movementDirection;
         }
     }
 }
