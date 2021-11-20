ebf6a99a522e71ad7c79e1be58a766a1f9f19dcd
Benedek Balint Toth <benedek.toth@bookrkids.hu>
2021-11-20 07:51:11 +0100

    lava collision fix again

--- Top Down Shooter/Assets/Scripts/Lava.cs
+++ Top Down Shooter/Assets/Scripts/Lava.cs
@@ -6,19 +6,7 @@ public class Lava : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Collider2D lavaCollider = gameObject.GetComponent<Collider2D>();
-        if (collision.gameObject.CompareTag("Bullet"))
-            Destroy(collision.gameObject);
         if (collision.gameObject.CompareTag("asd"))
-            lavaCollider.isTrigger = false;
-    }
-
-    void OnCollisionExit2D(Collision2D other)
-    {
-        Collider2D lavaCollider = gameObject.GetComponent<Collider2D>();
-        if (other.gameObject.CompareTag("asd"))
-        {
-            lavaCollider.isTrigger = true;
-        }
+            Destroy(collision.gameObject);
     }
 }
