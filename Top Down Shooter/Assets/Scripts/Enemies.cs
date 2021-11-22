e7e30aabec3cceb2617e6795a3e2c49d56f421cd
per0ken <peter.lukacs16@gmail.com>
2021-11-22 16:31:34 +0100

    new

--- Top Down Shooter/Assets/Scripts/Enemies.cs
+++ Top Down Shooter/Assets/Scripts/Enemies.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public GameObject Player;
 
+    private int lives = 3;
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(collision.gameObject);
-        Destroy(this.gameObject);
+        if (collision.gameObject.CompareTag("Bullet")) lives--;
+        if (collision.gameObject.CompareTag("asd")) lives--;
+        if (collision.gameObject.CompareTag("Bullet")) Destroy(collision.gameObject);
+        if (collision.gameObject.CompareTag("asd")) Destroy(collision.gameObject);
+        if (lives==0) Destroy(this.gameObject);
     }
 
+    void FixedUpdate()
+    {
+        Vector2 Playerr = Player.transform.position;
+        Vector2 lookDir = Playerr - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
 }
