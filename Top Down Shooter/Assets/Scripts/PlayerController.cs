155dbc53692d7dd46b76d1d3555cae854c8187ec
per0ken <peter.lukacs16@gmail.com>
2021-11-23 00:30:14 +0100

    enemies shooting

--- Top Down Shooter/Assets/Scripts/PlayerController.cs
+++ Top Down Shooter/Assets/Scripts/PlayerController.cs
@@ -12,11 +12,14 @@ public class PlayerController : MonoBehaviour
     Vector2 movement;
     Vector2 mousePos;
 
-    // Start is called before the first frame update
-    void Start()
+    private int lives = 3;
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        //if (collision.gameObject.CompareTag("EnemyBullet")) lives--;
+        if (collision.gameObject.CompareTag("EnemyBullet")) Destroy(collision.gameObject);
+        if (lives==0) Destroy(this.gameObject);
     }
+
 
     // Update is called once per frame
     void Update()
@@ -27,6 +30,9 @@ public class PlayerController : MonoBehaviour
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
+
+    
+
 
     void FixedUpdate()
     {
---
+++ Top Down Shooter/Assets/Scripts/enemyShooting.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyShooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private float bulletForce = 12.5f;
+
+    private float minTime = 1.0f;
+    private float maxTime = 3.0f;
+
+    public float spawnTimeInternal;
+
+    private float timer;
+
+    void Start()
+    {
+
+        InvokeRepeating("Shoot", 1.5f,spawnTimeInternal);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > spawnTimeInternal)
+        {
+            Shoot();
+            spawnTimeInternal = Random.Range(minTime, maxTime);
+            timer = 0;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        Object.Destroy(bullet, 1.5f);
+    }
+}
