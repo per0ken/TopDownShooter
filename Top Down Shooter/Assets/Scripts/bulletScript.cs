b4e190c7321800759d6cc8d2aca7fce28829e84d
Benedek Balint Toth <benedek.toth@bookrkids.hu>
2021-11-23 22:08:03 +0100

    Bullet stops when hits rock or another Player-fired Bullet instance

--- Top Down Shooter/Assets/Scripts/bulletScript.cs
+++ Top Down Shooter/Assets/Scripts/bulletScript.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class bulletScript : MonoBehaviour
 {
     CapsuleCollider2D ObjectCollider;
+    Rigidbody2D rigidbody;
 
     private void Start()
     {
         ObjectCollider = GetComponent<CapsuleCollider2D>();
-    }/*
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Rock") || collision.CompareTag("Bullet")) // when hits a rock or another Player-fired Bullet instance it stops the current (aka Bullet) gameObject
+            rigidbody.velocity = Vector3.zero;
+    }
+
+    /*
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("EnemyBullet")) ObjectCollider.isTrigger = true;
